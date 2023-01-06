fd879d1b99126561da96dff1a7aca883d221601e
Douglas <dev.douggo@gmail.com>
2023-01-05 21:49:02 -0300

    Altera configurações do repositório no github

--- method-references/src/main/java/Main.java
+++ method-references/src/main/java/Main.java
@@ -4,7 +4,7 @@ import Service.TextService;
 public class Main {
 
     public static void main(String[] args) {
-        TextService service = EnumService.ANONYMOUS.getService();
+        TextService service = EnumService.ABSTRACT_METHOD_REFERENCE.getService();
         TextProvider provider = new TextProvider(service);
         provider.process();
     }
